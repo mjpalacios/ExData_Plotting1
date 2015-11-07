3fca0ce039d2b5b76b58a0ff21c8d58c59fe55a8
mjpalacios <manuel.jesus.palacios@gmail.com>
2015-11-07 12:57:28 -0600

    Initial commit for Project 1 in Coursera Data Science Specialization Exploratory Data Analysis Course

---
+++ plot2.R
@@ -0,0 +1,49 @@
+#Project 1
+
+##Plot 2
+plot2 <- function() {
+  ### 1. Read Raw Data
+  if (!file.exists("household_power_consumption.zip")) {
+    rc <-
+      download.file(
+        "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip", 
+        "household_power_consumption.zip"
+      )
+    if (rc != 0) {
+      stop("Couldn't download household power consumption data from the Internet")
+    }
+  }
+  conn <- unz("household_power_consumption.zip", "household_power_consumption.txt")
+  hpc.raw <-
+    read.csv(
+      conn, header = TRUE, sep = ";", colClasses =
+        c("character", "character", rep("numeric", 7)), comment.char = "", na.strings = "?"
+    )
+  closeAllConnections()
+  
+  ### 2. Convert first column (Date) from character to Date
+  hpc.raw$Date <- as.Date(hpc.raw$Date, "%d/%m/%Y")
+  
+  ### 3. Subset Data to range of interest 2007-02-01 2007-02-02
+  hpc.sub <-
+    hpc.raw[hpc.raw$Date >= "2007-02-01" &
+              hpc.raw$Date <= "2007-02-02",]
+  
+  ### 4. Convert second column (Time) from character to POSIXlt
+  hpc.sub$Time <-
+    strptime(paste(hpc.sub$Date, hpc.sub$Time), "%Y-%m-%d %H:%M:%S")
+  
+  ### 5. Plot
+  p2ylab <- "Global Active Power (kilowatts)"
+  png(file = "plot2.png")
+  plot(
+    hpc.sub$Time, y = hpc.sub$Global_active_power, type = "l", xlab = "", ylab = p2ylab
+  )
+  dev.off()
+  return (0)
+}
+
+## To use:
+###source("plot2.R")
+###plot2()
+## That will create the "plot2.png" file.
---
+++ plot4.R
@@ -0,0 +1,73 @@
+#Project 1
+
+##Plot 4
+plot4 <- function() {
+  ### 1. Read Raw Data
+  if (!file.exists("household_power_consumption.zip")) {
+    rc <-
+      download.file(
+        "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip", 
+        "household_power_consumption.zip"
+      )
+    if (rc != 0) {
+      stop("Couldn't download household power consumption data from the Internet")
+    }
+  }
+  conn <- unz("household_power_consumption.zip", "household_power_consumption.txt")
+  hpc.raw <-
+    read.csv(
+      conn, header = TRUE, sep = ";", colClasses =
+        c("character", "character", rep("numeric", 7)), comment.char = "", na.strings = "?"
+    )
+  closeAllConnections()
+  
+  ### 2. Convert first column (Date) from character to Date
+  hpc.raw$Date <- as.Date(hpc.raw$Date, "%d/%m/%Y")
+  
+  ### 3. Subset Data to range of interest 2007-02-01 2007-02-02
+  hpc.sub <-
+    hpc.raw[hpc.raw$Date >= "2007-02-01" &
+              hpc.raw$Date <= "2007-02-02",]
+  
+  ### 4. Convert second column (Time) from character to POSIXlt
+  hpc.sub$Time <-
+    strptime(paste(hpc.sub$Date, hpc.sub$Time), "%Y-%m-%d %H:%M:%S")
+  
+  ### 5. Plot
+  ### 5. 1 Set Output File
+  png(file = "plot4.png")
+  
+  ### 5.2 Set layout as 4 regions, row order
+  par(mfrow = c(2,2))
+  
+  ### 5.3 Draw Top Left Region
+  plot(
+    hpc.sub$Time, y = hpc.sub$Global_active_power, type = "l", xlab = "", ylab = "Global Active Power"
+  )
+  
+  ### 5.4 Draw Top Right Region
+  plot(
+    hpc.sub$Time, hpc.sub$Voltage, type = "l", xlab = "datetime", ylab = "Voltage"
+  )
+  
+  ### 5.5 Draw Bottom Left Region
+  plot(
+    hpc.sub$Time, y = hpc.sub$Sub_metering_1, type = "l", xlab = "", ylab = "Energy sub metering"
+  )
+  p3legend <-
+    c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3")
+  p3color  <- c("black", "red", "blue")
+  legend("topright", legend = p3legend, lwd = 1, col = p3color, bty = "n")
+  lines(hpc.sub$Time, y = hpc.sub$Sub_metering_2, col = "red")
+  lines(hpc.sub$Time, y = hpc.sub$Sub_metering_3, col = "blue")
+  
+  ### 5.6 Draw Bottom Right Region
+  plot(
+    hpc.sub$Time, hpc.sub$Global_reactive_power,type = "l", xlab = "datetime", ylab =
+      "Global_reactive_power"
+  )
+ 
+  ### 5.7 Close Graphics Device
+  dev.off()
+  return(0)
+}
